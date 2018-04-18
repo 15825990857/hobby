0184d51cb6f189b5cdd8341a000b6dcc39713656
ygs <776716858@qq.com>
2018-04-18 18:09:16 +0800

    xx

---
+++ hobby.Core/Mapping/UserMapping.cs
@@ -0,0 +1,20 @@
+﻿using hobby.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hobby.Core.Mapping
+{
+   public class UserMapping: EntityTypeConfiguration<User>
+    {
+        public UserMapping()
+        {
+            this.HasKey(p=>p.Id);
+            this.Property(p => p.Name).HasMaxLength(250).IsRequired();
+            this.Property(p => p.Pwd).HasMaxLength(250).IsRequired();
+        }
+    }
+}
---
+++ hobby.Core/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hobby.Core.Model
+{
+    [Description]
+   public class User
+    {
+        public int Id { get; set; }
+         [Required]
+         [MaxLength(250)]
+        public string Name { get; set; }
+        public string Pwd { get; set; }
+        public DateTime Addtime { get; set; }
+        public int Status { get; set; }
+      
+        
+    }
+}
