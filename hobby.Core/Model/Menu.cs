9641cfa3e98efeb6e04ef0b5e074531bdefe83be
ygs <776716858@qq.com>
2018-05-09 15:12:35 +0800

    mm

---
+++ hobby.Core/Model/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hobby.Core.Model
+{
+   public class Menu
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int  ParentID { get; set; }
+        public int Sort { get; set; }
+        public string  Action { get; set; }
+        public string Controller { get; set; }
+    }
+}
